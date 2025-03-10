6783afbbcf87e049c729a34a4f6a54164705fd3b
Pavlo <pavlo.rohozhyn@gmail.com>
2025-03-10 07:28:51 +0100

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,16 @@
+// pre define data
 let formData = { email: '', message: '' };
 
+// form
 const refs = {
-  form: document.querySelector('.feedback-form'),
+  form: document.querySelector('.form-feedback'),
 };
 
-const fillFormFields = form => {
+// field handler
+const formFields = form => {
   try {
     const formDataFromLS = JSON.parse(
-      localStorage.getItem('feedback-form-state')
+      localStorage.getItem('form-feedback-state')
     );
 
     if (formDataFromLS === null) {
@@ -23,27 +26,26 @@ const fillFormFields = form => {
     console.log(err);
   }
 };
+formFields(refs.form);
 
-fillFormFields(refs.form);
-
+// change - handler
 const fieldChangeHandler = ({ target: formField }) => {
   formData[formField.name] = formField.value.trim();
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem('form-feedback-state', JSON.stringify(formData));
 };
+// change - listener
+refs.form.addEventListener('input', fieldChangeHandler);
 
-const formSubmitHandler = event => {
-  event.preventDefault();
-
+// submit handler
+const formSubmitHandler = e => {
+  e.preventDefault();
   if (formData.email === '' || formData.message === '') {
-    alert('Fill please all fields');
+    alert('All form field are required.');
     return;
   }
-  console.log(formData);
-
-  event.target.reset();
+  e.target.reset();
   formData = { email: '', message: '' };
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem('form-feedback-state');
 };
-
-refs.form.addEventListener('input', fieldChangeHandler);
+// submit - listener
 refs.form.addEventListener('submit', formSubmitHandler);
